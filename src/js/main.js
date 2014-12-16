5cb672186675947d3e8fe27dcd013021d5ee6145
Antonio Pierro <antonio.pierro@gmail.com>
2014-12-16 12:05:08 +0100

    Added angulartics to get automatic pageview tracking

--- src/js/main.js
+++ src/js/main.js
@@ -7,6 +7,8 @@
         urlArgs: 'bust=' + Date.now(),
         paths: {
             angular: vendor_dir + 'angular/angular',
+            angulartics: vendor_dir + 'angulartics/src/angulartics',
+            angularticsGa: vendor_dir + 'angulartics/src/angulartics-ga',
             angularUiRouter: vendor_dir + 'angular-ui-router/release/angular-ui-router',
             uiRouterStyles: vendor_dir + 'angular-ui-router-styles/ui-router-styles',
             jquery: vendor_dir + 'jquery/dist/jquery.min',
@@ -18,7 +20,11 @@
             revealjs: {
                 exports: 'Reveal'
             },
-            angular : {exports : 'angular'},
+            angular : {
+                exports : 'angular'
+            },
+            angulartics : ['angular'],
+            angularticsGa: ['angular'],
             jquery: {exports: 'jquery'},
             angularUiRouter: ['angular'],
             uiRouterStyles: ['angular'],
